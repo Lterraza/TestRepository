0b4ee92623bcaed0a6a3d5784c218a437565042c
Lterraza <74749687+Lterraza@users.noreply.github.com>
2021-01-19 13:10:45 -0700

    Add files via upload
    something

---
+++ BakeryShoppingCart/BakeryShoppingCart/BakeryShoppingCart/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BakeryShoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = SumOfTwoVariables(1, 2);
+
+            Console.WriteLine(a);
+        }
+
+        static int SumOfTwoVariables(int value1, int value2)
+        {
+            int result = 0;
+
+            result = result + value1 + value2;
+
+            return result;
+
+        }
+    }
+}
